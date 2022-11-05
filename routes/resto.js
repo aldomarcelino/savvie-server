fb1fd06bbfae628d71bad43294e143d19e69e7e5
alwanWicaksono <alwanwicaksono1@gmail.com>
2022-11-06 00:35:23 +0700

    cms controller profile restaurant

--- routes/resto.js
+++ routes/resto.js
@@ -11,6 +11,7 @@ router.put("/food/:id", authorizationResto, Resto.editFood);
 router.patch("/food/food-status/:id", authorizationResto, Resto.statusFood);
 router.patch("/food/food-active/:id", authorizationResto, Resto.activeFood);
 
+router.get("/restaurants", Resto.myRestaurant)
 router.put("/restaurants", Resto.editRestaurant)
 router.delete("/restaurants", Resto.deleteRestaurant)
 
