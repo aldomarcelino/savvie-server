b1e65f92c432aa634582681240c77dac929d285e
adibwafi <adibwafi@gmail.com>
2022-11-05 22:47:48 +0700

    added search

--- __test__/search.test.js
+++ __test__/search.test.js
@@ -9,21 +9,21 @@ const user_access_token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJyZXNjdWVmb29kQGdtYWlsLmNvbSIsImlhdCI6MTY2NzY0OTE1Mn0.Sqkgx312hBggjPziUR-QqYZD4mf8Le70OfR_HEyjhG0";
 
 describe("Food Routes Test", () => {
-    describe("GET /search - return data all foods", () => {
-        test("200 Success get all foods data, return array", (done) => {
-            request(app)
-                .get("/search?=ayam")
-                .set({ access_token: user_access_token })
-                .then((response) => {
-                const { body, status } = response;
-                expect(status).toBe(200);
-                expect(body.food).toBeInstanceOf(Array);
-                expect(body.restaurant).toBeInstanceOf(Array);
-                done();
-                })
-                .catch((err) => {
-                done(err);
-                });
+    describe("GET /search - return data with matching search result", () => {
+        test("200 Success get foods and restaurant data with matching search result, return object", (done) => {
+        request(app)
+            .get("/search?=ayam")
+            .set({ access_token: user_access_token })
+            .then((response) => {
+            const { body, status } = response;
+            expect(status).toBe(200);
+            expect(body.food).toBeInstanceOf(Array);
+            expect(body.restaurant).toBeInstanceOf(Array);
+            done();
+            })
+            .catch((err) => {
+            done(err);
             });
-    })
-})
+        });
+    });
+});
