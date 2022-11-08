c1b3245a2ad30addbcd740c246347840020ee5b6
alwanWicaksono <alwanwicaksono1@gmail.com>
2022-11-08 21:09:28 +0700

    test

--- controllers/xendit.js
+++ controllers/xendit.js
@@ -1,60 +1,54 @@
-const XenditInvoice = require("../api/xendit");
-const { User, Balance, sequelize } = require("../models");
+const XenditInvoice = require('../api/xendit');
+const { User, Balance, sequelize } = require('../models');
 
 class Controller {
-  static async topUp(req, res, next) {
-    const t = await sequelize.transaction();
+  static async topUp (req, res, next) {
+    const t = await sequelize.transaction()
     try {
-      const { balance } = req.body;
-      const { id } = req.user;
-      const findUser = await User.findByPk(id, {
-        include: [Balance],
-      });
-      const xenditInvoice = await XenditInvoice.createInvoice(
-        findUser.id + "",
-        balance,
-        findUser
-      );
-      await t.commit();
-      res.status(200).json({
-        code: 200,
-        status: "success",
-        message: "please pay to continue",
-        data: xenditInvoice.invoice_url,
-      });
+        const { balance } = req.body
+        const { id } = req.user
+        const findUser = await User.findByPk(id, {
+            include:[Balance]
+        })
+        const xenditInvoice = await XenditInvoice.createInvoice(findUser.id+'', balance, findUser)
+        await t.commit()
+        res.status(200).json({
+            code: 200,
+            status: 'success',
+            message: 'please pay to continue',
+            data : xenditInvoice.invoice_url
+        })
     } catch (error) {
-      await t.rollback();
-      next(error);
+        await t.rollback();
+        next(error)
     }
   }
-  static async success(req, res, next) {
+  static async success(req, res, next){
     try {
-      //   console.log(req.body)
-      const findUser = await User.findByPk(externalId, {
-        include: [Balance],
-      });
-      //   console.log(findUser);
-      const findWallet = await Balance.findOne({
-        where: {
-          UserId: findUser.id,
-        },
-      });
-      //   console.log(findWallet);
-      await Balance.update(
-        {
-          balance: +findWallet.balance + +amount,
-        },
-        {
-          where: {
-            UserId: findUser.id,
-          },
-        }
-      );
-      res.status(201).json({ message: "topup success" });
+        let {amount, externalId} = req.params
+        console.log(req.params)
+        const findUser = await User.findByPk(externalId, {
+            include:[Balance]
+        })
+        console.log(findUser)
+        const findWallet = await Balance.findOne({
+            where: {
+                UserId : findUser.id
+            }
+        })
+        console.log(findWallet)
+        await Balance.update({
+            balance : +findWallet.balance + +amount
+        }, {
+            where: {
+                UserId: findUser.id
+            }
+        })
+        res.status(201).json({"message": "topup success"})
     } catch (error) {
-      next(error);
+        
     }
   }
 }
 
-module.exports = Controller;
+module.exports = Controller
